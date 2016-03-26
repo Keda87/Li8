7102ec64095f2b9d2124940960975d54d2b220e7
adiyatmubarak <adiyatmubarak@gmail.com>
2016-03-26 09:48:49 +0700

    feat: Add jinja2 template engine and refactor to class based wsgi

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 from wsgiref.simple_server import make_server
 
-from core.server import application
+from core.server import Li8App
 
 
 if __name__ == '__main__':
+    application = Li8App()
     # Example to using middleware
     # application = ErrorMiddleware(app=application)
-
     try:
         httpd = make_server('127.0.0.1', 8081, application)
         banner = 'Li8 web framework, running at %s:%d\n'\
--- core/server.py
+++ core/server.py
@@ -1,19 +1,23 @@
 from .utils import get_template
 
 
-def application(environ, start_response):
-    """
-    Reference: http://wsgi.tutorial.codepoint.net/application-interface
-    :environ        -- CGI environment variables.
-    :start_response -- a callback function supplied by the server which takes
-                       the HTTP status and headers as arguments.
-    """
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    with open(get_template('default.html')) as template:
-        return [template.read().encode()]
+class Li8App(object):
+
+    def __call__(self, environ, start_response):
+        """
+        Reference: http://wsgi.tutorial.codepoint.net/application-interface
+        :environ        -- CGI environment variables.
+        :start_response -- a callback function supplied by the server which takes
+                           the HTTP status and headers as arguments.
+        """
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        with open(get_template('default.html')) as template:
+            return [template.read().encode()]
 
 
-def not_found(environ, start_response):
-    message = b'Page that you looking found was not found.'
-    start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
-    return [message]
+class Li8NotFoundApp(Li8App):
+
+    def __call__(self, environ, start_response):
+        start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
+        with open(get_template('404.html')) as template:
+            return [template.read().encode()]
